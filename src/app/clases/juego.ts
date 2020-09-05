809e557ec992fe9c313ee1f2445fad827b483d31
Seba1705 <sebaaguirre2012@gmail.com>
2020-09-04 21:10:40 -0300

    Prueba en heroku

--- src/app/clases/juego.ts
+++ src/app/clases/juego.ts
@@ -1,27 +1,22 @@
 export abstract class Juego {
-  public nombre = 'Sin Nombre';
-  public jugador: string;
-  public gano = false;
+    public nombre = 'Sin Nombre';
+    public jugador: string;
+    public gano = false;
 
-  constructor(nombre?: string, gano?: boolean,jugador?:string) {
-    if (nombre)
-      this.nombre = nombre;
+    constructor(nombre?: string, gano?: boolean,jugador?:string) {
+        if (nombre)
+            this.nombre = nombre;
+        if (gano)
+            this.gano = gano;
+        if(jugador)
+            this.jugador=jugador;
+        else
+            this.jugador= "natalia natalia";
+    }
 
-    if (gano)
-      this.gano = gano;
-    if(jugador)
-      this.jugador=jugador;
-    else
-      this.jugador= "natalia natalia";
-  }
-
-
-  
-
-  public abstract verificar():boolean; 
-  
-  public retornarAyuda() {
+    public abstract verificar():boolean; 
     
-    return "NO hay Ayuda definida";
-  }
+    public retornarAyuda() {
+        return "NO hay Ayuda definida";
+    }
 }
