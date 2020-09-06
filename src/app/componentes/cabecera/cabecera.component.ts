eccdc9b2e4d94cccbd0fcfbbe6573ac56f8bfde3
seba1705 <sebaaguirre2012@gmail.com>
2020-09-06 16:30:56 -0300

    cambiando estilos

--- src/app/componentes/cabecera/cabecera.component.ts
+++ src/app/componentes/cabecera/cabecera.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cabecera',
-  templateUrl: './cabecera.component.html',
-  styleUrls: ['./cabecera.component.css']
+    selector: 'app-cabecera',
+    templateUrl: './cabecera.component.html',
+    styleUrls: ['./cabecera.component.css']
 })
 export class CabeceraComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
