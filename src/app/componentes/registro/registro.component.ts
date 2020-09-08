55bbd8d3a800bc074d30ddc29fc6c5edeb78ed53
seba1705 <sebaaguirre2012@gmail.com>
2020-09-08 20:36:03 -0300

    Cambio de imagenes + registro

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { NgForm } from '@angular/forms';
 //para poder hacer las validaciones
 //import { Validators, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+    selector: 'app-registro',
+    templateUrl: './registro.component.html',
+    styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
 
- /* constructor( private miConstructor:FormBuilder) { }
-  email=new FormControl('',[Validators.email]);
-  formRegistro:FormGroup=this.miConstructor.group({
-    usuario:this.email
-  });*/
-  constructor( ) { }
+  /* constructor( private miConstructor:FormBuilder) { }
+    email=new FormControl('',[Validators.email]);
+    formRegistro:FormGroup=this.miConstructor.group({
+      usuario:this.email
+    });*/
 
-  ngOnInit() {
-  }
+
+    usuario = {
+        email: '',
+        clave: '',
+        reclave: ''
+    };
+      
+    constructor(private router: Router) { }
+
+    ngOnInit(): void {
+    }
+
+    registro(form: NgForm) {
+
+        if (form.invalid)
+            Object.values(form.controls).forEach(control => control.markAsTouched());
+        else 
+          this.router.navigateByUrl('/Login');
+
+    }
 
 }
