a9f8bcd66061ca5b1a6469491355da882701f7b2
seba1705 <sebaaguirre2012@gmail.com>
2020-09-13 22:17:42 -0300

    Cambio de estilo home - juegos

--- src/app/componentes/quien-soy/quien-soy.component.ts
+++ src/app/componentes/quien-soy/quien-soy.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-quien-soy',
-  templateUrl: './quien-soy.component.html',
-  styleUrls: ['./quien-soy.component.css']
+    selector: 'app-quien-soy',
+    templateUrl: './quien-soy.component.html',
+    styleUrls: ['./quien-soy.component.css']
 })
 export class QuienSoyComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
